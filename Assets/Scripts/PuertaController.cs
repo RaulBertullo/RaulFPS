c86a9b0dd3b94940331621f642cb46a5dd45477f
RaulBertullo <rabeal@alumnatflorida.es>
2026-01-15 03:24:42 +0100

    comentarios
    He borrado comentarios que he ido escribiendo a mí mismo porque ya tengo claro para que era cada cosa.

--- Assets/Scripts/PuertaController.cs
+++ Assets/Scripts/PuertaController.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class PuertaController : MonoBehaviour
 {
-    public Animator doorAnimator; // arrastras el Animator de la puerta
+    public Animator doorAnimator; 
 
-    // Este método lo llamas desde el SimonController
+   
     public void OpenDoor()
     {
         if (doorAnimator != null)
         {
-            doorAnimator.SetTrigger("Open"); // dispara la animación
+            doorAnimator.SetTrigger("Open"); 
         }
     }
 }
